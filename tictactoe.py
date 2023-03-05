44d1dbc9f6812eda8a1c18b9c79cd810011a8d2f
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-05 13:31:47 -0600

    new and improved TicTacToe class

--- tictactoe.py
+++ tictactoe.py
@@ -1,31 +1,39 @@
 # flake8: noqa
-
-# TicTacToe implementation for abstract Node
-
-from random import choice
-from .mcts import Node
+import numpy as np
 
 
-class TicTacToe(Node):
-    @staticmethod
-    def get_initial_state():
-        return [0] * 9
+class TicTacToe:
+    def __init__(self):
+        self.size = 3
+        self.action_space = self.size * self.size
 
-    def _try_expand(self):
-        moves = self.get_next_moves(self.state)
-        if not moves:
-            return
-        return TicTacToe(choice(moves), self)
+    def get_initial_state(self):
+        return np.zeros(self.action_space)
 
-    def get_next_moves(self, state):
-        ans = []
-        for i, v in enumerate(state):
-            if v == 0:
-                state[i] = -1
-                for j, v2 in enumerate(state):
-                    if v2 == 0:
-                        state[j] = 1
-                        ans.append(state.copy())
-                        state[j] = 0
-                state[i] = 0
-        return ans
+    def get_valid_moves(self, state):
+        return (state == 0).astype(np.int8)
+
+    def get_next_state(self, state, action, player):
+        state[action] = player
+        return state
+
+    def _check_win(self, state, action):
+        row = action // self.size
+        col = action % self.size
+        goal = self.size * state[action]
+        return (
+            state[row, :].sum() == goal
+            or state[:, col].sum() == goal
+            or state.trace() == goal
+            or np.flip(state, axis=0).trace() == goal
+        )
+
+    def get_value_and_terminated(self, state, action):
+        if self._check_win(state, action):
+            return 1, True
+        if state.all():  # check if all elements are non-zero
+            return 0, True
+        return 0, False
+
+    def get_opponent(self, player):
+        return -player
