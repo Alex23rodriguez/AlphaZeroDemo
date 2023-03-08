8b73e20c22fc917a05f5d7edd1d62ff7322df663
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-08 00:57:37 -0600

    modified tictactoe to inherit from Game and use correct type hints

--- tictactoe.py
+++ tictactoe.py
@@ -1,23 +1,24 @@
 # flake8: noqa
 import numpy as np
+from game import Game, Moves, State
 
 
-class TicTacToe:
+class TicTacToe(Game):
     def __init__(self, size=3):
         self.size = size
         self.action_space = self.size * self.size
 
-    def get_initial_state(self):
+    def get_initial_state(self) -> State:
         return np.zeros((self.size, self.size), np.int8)
 
-    def get_valid_moves(self, state):
+    def get_valid_moves(self, state: State) -> Moves:
         return (state.reshape(-1) == 0).astype(np.uint8)
 
-    def get_next_state(self, state, action, player):
+    def get_next_state(self, state: State, action: int, player: int) -> State:
         state.reshape(-1)[action] = player
         return state
 
-    def _check_win(self, state, action):
+    def _check_win(self, state: State, action: int) -> bool:
         row = action // self.size
         col = action % self.size
         goal = self.size * state.reshape(-1)[action]
@@ -28,12 +29,12 @@ class TicTacToe:
             or np.flip(state, axis=0).trace() == goal
         )
 
-    def get_value_and_terminated(self, state, action):
+    def get_value_and_terminated(self, state: State, action: int) -> tuple[int, bool]:
         if self._check_win(state, action):
             return 1, True
         if state.all():  # check if all elements are non-zero
             return 0, True
         return 0, False
 
-    def get_opponent(self, player):
+    def get_opponent(self, player: int) -> int:
         return -player
